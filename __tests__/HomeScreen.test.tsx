d5cda3f77bf981e56b4460ec61b5a185107e0bb9
jsayago77 <jsayago.dev@gmail.com>
2025-03-18 21:53:40 -0400

    Refactor HomeScreen tests to simplify structure and verify rendering of "Pantalla Home" text

--- __tests__/HomeScreen.test.tsx
+++ __tests__/HomeScreen.test.tsx
@@ -1,70 +1,13 @@
+// __tests__/HomeScreen.test.tsx
 import React from 'react';
 import { render, waitFor } from '@testing-library/react-native';
 import HomeScreen from '../src/screens/tab/HomeScreen';
-import { ProductService } from '../src/services/products';
 
-// Mock de ProductService
-jest.mock('../src/services/products', () => ({
-  ProductService: {
-    getProducts: jest.fn(),
-  },
-}));
-
-describe('HomeScreen', () => {
-  beforeEach(() => {
-    // Limpiar todos los mocks antes de cada prueba
-    jest.clearAllMocks();
-  });
-
-  it('debería renderizar correctamente el componente', async () => {
-    // Mock de la respuesta exitosa de getProducts
-    (ProductService.getProducts as jest.Mock).mockResolvedValue({
-      success: true,
-      data: {
-        results: [
-          {
-            genericName: 'Producto 1',
-            images: [{ url: 'http://example.com/image1.jpg' }],
-            categories: [{ name: 'Category 1' }],
-          },
-          {
-            genericName: 'Producto 2',
-            images: [{ url: 'http://example.com/image2.jpg' }],
-            categories: [{ name: 'Category 2' }],
-          },
-        ],
-      },
-    });
-
-    const { getByTestId, getByText } = render(<HomeScreen />);
-
-    // Verificar que el componente se renderiza correctamente
-    expect(getByTestId('home-screen')).toBeTruthy();
-
-    // Esperar a que los productos se carguen
+describe('<HomeScreen />', () => {
+  test('renders "Pantalla Home" text', async () => {
+    const { getByText } = render(<HomeScreen />);
     await waitFor(() => {
-      expect(getByText('Ofertas especiales')).toBeTruthy();
-      expect(getByText('Medicamentos')).toBeTruthy();
-    });
-  });
-
-  it('debería manejar el error al obtener los productos', async () => {
-    // Mock de la respuesta fallida de getProducts
-    (ProductService.getProducts as jest.Mock).mockResolvedValue({
-      success: false,
-      error: 'Error al obtener los productos',
-    });
-
-    const { getByTestId } = render(<HomeScreen />);
-
-    // Verificar que el componente se renderiza correctamente
-    expect(getByTestId('home-screen')).toBeTruthy();
-
-    // Esperar a que se maneje el error
-    await waitFor(() => {
-      expect(console.log).toHaveBeenCalledWith(
-        'Error al obtener los productos',
-      );
+      expect(getByText('Pantalla Home')).toBeTruthy();
     });
   });
 });
