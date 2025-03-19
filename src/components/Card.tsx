b0ecd9f450a49b510bc8737248e3095193da738a
jsayago77 <jsayago.dev@gmail.com>
2025-03-18 21:50:07 -0400

    Add null check for text in truncateString function

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,8 +5,10 @@ import PoppinsText from './PoppinsText';
 import CardButton from './CardButton';
 
 const truncateString = (text: string, maxLength: number = 27): string => {
-  if (text.length > maxLength) {
-    return text.substring(0, maxLength) + '...';
+  if (text) {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + '...';
+    }
   }
   return text;
 };
