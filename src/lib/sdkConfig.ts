a2c65a2e245ba5bae1ef732f34ba789d6ade4a50
miglesiasDev <1001.28391672.ucla@gmail.com>
2025-03-25 11:25:16 -0400

    Using just one api instance

--- src/lib/sdkConfig.ts
+++ src/lib/sdkConfig.ts
@@ -4,12 +4,9 @@ import { PHARMATECH_DEV_MODE } from '@env';
 const isDevelopment = PHARMATECH_DEV_MODE === 'true';
 
 // Instancia del SDK para solicitudes con Origin
-export const apiWithOrigin = new PharmaTech(
+export const api = new PharmaTech(
   isDevelopment,
   isDevelopment
     ? 'http://localhost:3000' // Origin para desarrollo
     : 'https://pharmatech.site', // Origin para producción
 );
-
-// Instancia del SDK para solicitudes sin Origin
-export const apiWithoutOrigin = new PharmaTech(isDevelopment);
