ce1dfb68b96ca0137e513141207318f61a1ef8da
miglesiasDev <1001.28391672.ucla@gmail.com>
2025-04-04 17:04:04 -0400

    Configuring google maps integration

--- src/screens/tab/BranchesScreen.tsx
+++ src/screens/tab/BranchesScreen.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import PoppinsText from '../../components/PoppinsText';
 import { Colors } from '../../styles/theme';
+import MapViewComponent from '../../components/MapViewComponent';
 
 export default function BranchesScreen() {
   return (
     <View style={styles.container}>
       <PoppinsText>Pantalla Sucursales</PoppinsText>
+      <View style={styles.mapContainer}>
+        <MapViewComponent
+          initialRegion={{
+            latitude: 37.7749,
+            longitude: -122.4194,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +25,9 @@ export default function BranchesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: Colors.bgColor,
+  },
+  mapContainer: {
+    flex: 1, // Asegura que el mapa ocupe todo el espacio disponible
   },
 });
