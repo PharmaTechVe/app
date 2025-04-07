c7b04b590386954033d035b7c171dde09fbcd1f4
miglesiasDev <1001.28391672.ucla@gmail.com>
2025-04-07 11:52:17 -0400

    Eliminating unnecessary map load in branches screen

--- src/screens/tab/BranchesScreen.tsx
+++ src/screens/tab/BranchesScreen.tsx
@@ -2,22 +2,11 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import PoppinsText from '../../components/PoppinsText';
 import { Colors } from '../../styles/theme';
-import MapViewComponent from '../../components/MapViewComponent';
 
 export default function BranchesScreen() {
   return (
     <View style={styles.container}>
       <PoppinsText>Pantalla Sucursales</PoppinsText>
-      <View style={styles.mapContainer}>
-        <MapViewComponent
-          initialRegion={{
-            latitude: 37.7749,
-            longitude: -122.4194,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
     </View>
   );
 }
@@ -26,8 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.bgColor,
-  },
-  mapContainer: {
-    flex: 1, // Asegura que el mapa ocupe todo el espacio disponible
   },
 });
