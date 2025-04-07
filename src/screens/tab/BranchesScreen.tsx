4b0a0474d100198d06d9c2a034cb95f9de815841
miglesiasDev <1001.28391672.ucla@gmail.com>
2025-04-07 11:54:19 -0400

    Adding styles I forgot to put

--- src/screens/tab/BranchesScreen.tsx
+++ src/screens/tab/BranchesScreen.tsx
@@ -14,6 +14,8 @@ export default function BranchesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: Colors.bgColor,
   },
 });
