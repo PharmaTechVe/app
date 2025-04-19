4865ea4e7603babfaeafadb5c953218ca13b463b
jsayago77 <jsayago.dev@gmail.com>
2025-04-19 18:58:58 -0400

    Changed Avatar fontSize

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Image, View, StyleSheet, ActivityIndicator } from 'react-native';
-import { Colors, FontSizes } from '../styles/theme';
+import { Colors } from '../styles/theme';
 import { UserService } from '../services/user';
 import PoppinsText from './PoppinsText';
 import * as SecureStore from 'expo-secure-store';
 
 type AvatarProps = {
-  scale?: number;
+  scale?: number; // Propiedad para definir el tamaño del avatar
 };
 
 const Avatar: React.FC<AvatarProps> = ({ scale = 32 }) => {
@@ -23,7 +23,6 @@ const Avatar: React.FC<AvatarProps> = ({ scale = 32 }) => {
           'No se encontró el token de autenticación. Usando datos locales.',
         );
 
-        // Data from secureStore
         const userData = await SecureStore.getItemAsync('user_data');
         if (userData) {
           const { firstName, lastName } = JSON.parse(userData);
@@ -56,26 +55,62 @@ const Avatar: React.FC<AvatarProps> = ({ scale = 32 }) => {
   }, []);
 
   const getInitials = (fullName: string): string => {
-    console.log('Full Name:', fullName);
     const words = fullName.trim().split(' ');
     if (words.length === 1) return words[0].charAt(0).toUpperCase();
     return (words[0].charAt(0) + words[1].charAt(0)).toUpperCase();
   };
 
+  // Cálculo dinámico de tamaño y fuentes
+  const avatarSize = scale;
+  const fontSize = avatarSize / 2.5;
+  const lineHeight = fontSize + 1;
+  const marginTop = avatarSize / 10;
+
   if (loading) {
     return (
-      <View style={[styles.avatarContainer, styles.loadingContainer]}>
+      <View
+        style={[
+          styles.avatarContainer,
+          styles.loadingContainer,
+          {
+            width: avatarSize,
+            height: avatarSize,
+            borderRadius: avatarSize / 2,
+          },
+        ]}
+      >
         <ActivityIndicator color={Colors.textWhite} />
       </View>
     );
   }
 
   return (
-    <View style={[styles.avatar, { width: scale, height: scale }]}>
+    <View
+      style={[
+        styles.avatar,
+        { width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2 },
+      ]}
+    >
       {profile.uri ? (
-        <Image source={{ uri: profile.uri }} style={styles.avatarImage} />
+        <Image
+          source={{ uri: profile.uri }}
+          style={[
+            styles.avatarImage,
+            { width: avatarSize, height: avatarSize },
+          ]}
+        />
       ) : (
-        <PoppinsText style={styles.initials} weight="semibold">
+        <PoppinsText
+          style={[
+            styles.initials,
+            {
+              fontSize: fontSize,
+              lineHeight: lineHeight,
+              marginTop: marginTop,
+            },
+          ]}
+          weight="semibold"
+        >
           {profile.name ? getInitials(profile.name) : ''}
         </PoppinsText>
       )}
@@ -85,21 +120,17 @@ const Avatar: React.FC<AvatarProps> = ({ scale = 32 }) => {
 
 const styles = StyleSheet.create({
   avatar: {
-    borderRadius: 100,
     backgroundColor: Colors.primary,
     alignItems: 'center',
     justifyContent: 'center',
     overflow: 'hidden',
   },
   avatarImage: {
-    width: 32,
-    height: 32,
     resizeMode: 'cover',
   },
   initials: {
-    fontSize: FontSizes.label.size,
-    lineHeight: FontSizes.label.lineHeight,
     color: Colors.textWhite,
+    textAlign: 'center',
   },
   avatarContainer: {
     backgroundColor: Colors.primary,
