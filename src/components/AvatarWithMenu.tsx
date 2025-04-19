1b5efbf3e2046c7ae0a8061dfb0cfb5067ab6932
jsayago77 <jsayago.dev@gmail.com>
2025-04-19 00:58:16 -0400

    Using menu as screen

--- src/components/AvatarWithMenu.tsx
+++ src/components/AvatarWithMenu.tsx
@@ -1,56 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
-import SlideMenu from './SlideMenu';
 import Avatar from './Avatar';
-import {
-  UserIcon,
-  MapIcon,
-  ShieldCheckIcon,
-  ShoppingCartIcon,
-} from 'react-native-heroicons/outline';
-import { Colors } from '../styles/theme';
 import { useRouter } from 'expo-router';
 
 const AvatarWithMenu: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
-
-  const menuItems = [
-    {
-      id: '1',
-      title: 'Perfil',
-      icon: <UserIcon color={Colors.iconMainPrimary} />,
-      onPress: () => router.push('/profile'),
-    },
-    {
-      id: '2',
-      title: 'Mis Direcciones',
-      icon: <MapIcon color={Colors.iconMainPrimary} />,
-      onPress: () => router.push('/direction'),
-    },
-    {
-      id: '3',
-      title: 'Seguridad',
-      icon: <ShieldCheckIcon color={Colors.iconMainPrimary} />,
-      onPress: () => router.push('/change-password'),
-    },
-    {
-      id: '4',
-      title: 'Mis Pedidos',
-      icon: <ShoppingCartIcon color={Colors.iconMainPrimary} />,
-      onPress: () => router.push('/orders'),
-    },
-  ];
 
   return (
     <View style={styles.container}>
-      <SlideMenu
-        isOpen={isMenuOpen}
-        onClose={() => setIsMenuOpen(false)}
-        items={menuItems}
-      />
-
-      <TouchableOpacity onPress={() => setIsMenuOpen(!isMenuOpen)}>
+      <TouchableOpacity onPress={() => router.push('menu')}>
         <Avatar />
       </TouchableOpacity>
     </View>
