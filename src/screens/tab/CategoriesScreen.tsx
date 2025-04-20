d69e856aa6464764b639339cbd3512020b98b72b
jsayago77 <jsayago.dev@gmail.com>
2025-04-19 20:21:08 -0400

    Added categories service

--- src/screens/tab/CategoriesScreen.tsx
+++ src/screens/tab/CategoriesScreen.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import PoppinsText from '../../components/PoppinsText';
 import { Colors } from '../../styles/theme';
+import { CategoryService } from '../../services/category';
 
 export default function CategoriesScreen() {
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const categories = await CategoryService.getCategories(1, 100);
+      if (categories.success) {
+        console.log(categories.data);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
   return (
     <View style={styles.container}>
       <PoppinsText>Pantalla Categorías</PoppinsText>
