7d2f43dbd17ffbd0ce4c05a9b7e320e8d647884a
miglesiasDev <1001.28391672.ucla@gmail.com>
2025-04-26 14:04:04 -0400

    Fixing the way maps use the key

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,3 +1,7 @@
 declare module '@env' {
   export const PHARMATECH_DEV_MODE: string;
+  export const GOOGLE_MAPS_API_KEY: string;
+  export const NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: string;
+  export const CLOUDINARY_API_KEY: string;
+  export const CLOUDINARY_API_SECRET: string;
 }
