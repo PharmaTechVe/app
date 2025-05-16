88c0e1e8fbbfac95e7dffaf928d7aa7b7a49f3a7
miglesiasDev <1001.28391672.ucla@gmail.com>
2025-05-16 11:26:28 -0400

    babel.config fix

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,22 @@
-const isProduction = process.env.NODE_ENV === 'production';
-const isDevelopmentBuild = process.env.EAS_BUILD_PROFILE === 'development';
-
 module.exports = function (api) {
   api.cache(true);
-
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
     plugins: [
-      !isProduction &&
-        !isDevelopmentBuild && [
-          'module:react-native-dotenv',
-          {
-            moduleName: '@env',
-            path: '.env',
-            blacklist: null,
-            whitelist: null,
-            safe: false,
-            allowUndefined: true,
-          },
-        ],
-    ].filter(Boolean),
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
